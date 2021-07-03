2ce8366bd545b1660a71e8221f48e9ff43bc303c
STEEVEN DAVID ARIAS TOMALA <steeven.ariast@ug.edu.ec>
2021-07-02 22:58:21 -0500

    Agrego Taller Aldo Haz

---
+++ AldoHaz/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AldoHaz")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AldoHaz")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("825e99ef-1e5d-4399-ab42-7fec0a889fd6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AldoHaz/UnitTest.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Threading;
+
+namespace AldoHaz
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string url = "https://admin-sysnnova.com/OpenFact/Account/Login.aspx?AspxAutoDetectCookieSupport=1";
+            ChromeDriver chrdrv = new ChromeDriver();
+            chrdrv.Navigate().GoToUrl(url);
+            chrdrv.Manage().Window.Maximize();
+            chrdrv.FindElement(By.Id("LoginUser_UserName")).SendKeys("demo");
+            chrdrv.FindElement(By.Id("LoginUser_Password")).SendKeys("0430");
+            chrdrv.FindElement(By.Name("LoginUser$LoginButton")).Click();
+            Thread.Sleep(2000);
+            chrdrv.FindElement(By.Id("liClientes")).Click();
+            Thread.Sleep(3000);
+            chrdrv.FindElement(By.Id("MainContent_btnAdd")).Click();
+            Thread.Sleep(5000);
+            chrdrv.FindElement(By.Id("MainContent_txtNombreCliente")).SendKeys("AldoHaz");
+            var chrdrvIdentidad = chrdrv.FindElement(By.Id("MainContent_ddlTipoIdentificacion"));
+            var elem = new SelectElement(chrdrvIdentidad);
+            elem.SelectByText("Cédula");
+            Thread.Sleep(3000);
+            chrdrv.FindElement(By.Id("MainContent_txtIdentificacion")).SendKeys("0915272116");
+            chrdrv.FindElement(By.Id("MainContent_txtTelefonoConvencional")).SendKeys("5016948");
+            chrdrv.FindElement(By.Id("MainContent_txtExtension")).SendKeys("1018");
+            chrdrv.FindElement(By.Id("MainContent_txtTelefonoCelular")).SendKeys("0992795517");
+            chrdrv.FindElement(By.Id("MainContent_txtDireccion")).SendKeys("Alborada");
+            chrdrv.FindElement(By.Id("MainContent_txtMailDefecto")).SendKeys("aldo.haz@gmail.com");
+            chrdrv.FindElement(By.Id("MainContent_txtCorreosCopiaCliente")).SendKeys("aldo.haz.poke@gmail.com");
+            chrdrv.FindElement(By.Id("MainContent_btnGuardarCliente")).Click();
+            Thread.Sleep(5000);
+            chrdrv.SwitchTo().Alert().Accept();
+        }
+    }
+}
